71c5122c2867289294b94bdf5054fc7f1022ad5d
Fernando27271 <fernando.gchoyv@gmail.com>
2021-05-13 16:14:08 -0500

    TAREA 2
    TAREA 2

---
+++ process-form.php
@@ -0,0 +1,19 @@
+<?php
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+    $country = $_POST['country'];
+    $sexo = $_POST['sexo'];
+    $checkbox = $_POST['checkbox'];
+    $formFile = $_POST['formFile'];
+    $textarea = $_POST['textarea'];
+
+
+    echo "Nombre de usuario: $username
+    <br>Contraseña: $password
+    <br>Lugar de residencia: $country
+    <br>Sexo: $sexo
+    
+    <br><br>$username ha publicado el siguiente archivo: $formFile 
+    <br>$username ha realizado el siguiente comentario: '$textarea'.
+    <br>$username $checkbox.";
+?>
